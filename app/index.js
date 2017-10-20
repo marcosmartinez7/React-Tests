4c08b28acb674fb6de6e10594cdbaa3a8371dc11
Marcos Martinez <mmartinezciompi@gmail.com>
2017-10-20 18:00:45 -0300

    all the examples in one page

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-/*
+
 var WelcomeHandler = require('./components/WelcomeHandler');
 
 
@@ -12,69 +12,59 @@ ReactDOM.render(
     document.getElementById('app')
 );
 
-*/
 
-/**
- * var App = require('./components/App');
 
-    const app = <App />;    
+  var App = require('./components/App');
+
+    const app1 = <App />;    
     ReactDOM.render(
-        app,
-        document.getElementById('app')
+        app1,
+        document.getElementById('app1')
     );
- */
 
- /**
-  * var LoginController = require('./components/LoginController');
+   var LoginController = require('./components/LoginController');
 
-    const app = <LoginController />;    
+    const app2 = <LoginController />;    
     ReactDOM.render(
-        app,
-        document.getElementById('app')
+        app2,
+        document.getElementById('app2')
     );
-  */
 
-  /**
-   * var Page = require('./components/Page');
+    var Page = require('./components/Page');
 
-        const app = <Page />;
+        const app3 = <Page />;
         ReactDOM.render(
-            app,
-            document.getElementById('app')
+            app3,
+            document.getElementById('app3')
         );
-   */
 
-   /*
    var NumberList = require('./components/NumberList');
 
-   const app = <NumberList numbers={[1,2,3,4]}/>
+   const app4 = <NumberList numbers={[1,2,3,4]}/>
    ReactDOM.render(
-        app,
-        document.getElementById('app')
+        app4,
+        document.getElementById('app4')
     );
 
-   */
 
-   /**
-    *    var NameForm = require('./components/NameForm');
+  var NameForm = require('./components/NameForm');
 
-            const app = <NameForm />
+            const app5 = <NameForm />
             ReactDOM.render(
-                    app,
-                    document.getElementById('app')
+                    app5,
+                    document.getElementById('app5')
                 );
 
-    */
-
-/**
- *  var Reservation = require('./components/Reservation');
     
-       const app = <Reservation />
+
+ var Reservation = require('./components/Reservation');
+    
+       const app6 = <Reservation />
        ReactDOM.render(
-            app,
-            document.getElementById('app')
+            app6,
+            document.getElementById('app6')
         );
- */
+
    
     
 
